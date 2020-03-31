15e293b8efaa9bdc8b5423859e6968a42afb6840
andreymuskat <54885364+andreymuskat@users.noreply.github.com>
2020-03-31 12:20:53 +0300

    Update python.py

--- SQL-master/python.py
+++ SQL-master/python.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect, url_for, requ
 
 connection = pymysql.connect(host='localhost',
                                user='root',
-                               password='belka20050121'
+                               password='belka'
                                db='mydb',
                                charset='utf8mb4',
                                cursorclass=pymysql.cursors.DictCursor)
